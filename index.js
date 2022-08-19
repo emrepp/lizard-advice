ef78c926e26061e67c5b6fa63a31fcd7b4f803c2
Emma Repp <emmarepp.draws@gmail.com>
2022-08-18 19:23:58 -1000

    cleaned up code

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 
-   
-      fetch('https://api.kanye.rest/')
-      .then(resp => resp.json())
-      .then(data => {
-        
-   
   
-       console.log(data)
-      
-      });
-      
       async function getPosts() {
-
-     // const init = () => {
-
 
       Promise.all([
        
         fetch('https://api.kanye.rest/'),
-        fetch('https://api.adviceslip.com/advice')
-       
-        
+        fetch('https://api.adviceslip.com/advice') 
     
       ])
 
@@ -31,62 +16,30 @@
         }))
         
       }) 
-     
-    
-      
-
       
       .then(function(data){ 
-        //JSON.stringify(data);
-
-        
         const mySelect = document.getElementById('my-select');
         
        
           mySelect.addEventListener('change',(event) =>{
-          //event.preventDefault();
           const selected = mySelect.options[mySelect.selectedIndex].value;
-          console.log(selected)
           
-          
-         
   
 
           if (selected == 0) {
             const first = data.slice(0,1);
-            console.log(first)
-           
-          console.log( Object.values(first[0]))
 
-          
-       
-          
-            const quoteSpot = document.querySelector("h2#quote")
-            quoteSpot.textContent = Object.values(first[0])
-
-            
-           
-           
+            const quoteSpot = document.querySelector("h2#quote");
+            quoteSpot.textContent = Object.values(first[0]);
           }
         
           else {
-           const second = data.slice(1,2)
-           //second => addAdvice(second[0]['slip']['advice'])
-           console.log(second) 
-           console.log(Object.values(second[0].slip["advice"]))
-           const secondAdvice = Object.values(second[0].slip["advice"])
-
-           console.log(secondAdvice.join(''))
-
-          
-           const adviceThings = document.querySelector("h2#quote")
-
-           adviceThings.textContent = secondAdvice.join('')
-
-         
-    
-
+           const second = data.slice(1,2);
            
+           
+           const secondAdvice = Object.values(second[0].slip["advice"]);
+           const adviceThings = document.querySelector("h2#quote");
+           adviceThings.textContent = secondAdvice.join('');
 
           }
       
@@ -94,21 +47,13 @@
          
          }
 
-         const timer = setInterval(getPosts,3000)
+         const timer = setInterval(getPosts,3000);
          getPosts();
          
-       
-//document.addEventListener('DOMContentLoaded', init);
-
-
-
 
 
 const modal = document.getElementById("myModal");
-
-
 const btn = document.getElementById("myBtn");
-
 
 const span = document.getElementsByClassName("close")[0];
 
@@ -116,9 +61,6 @@ const span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
   
   modal.style.display = "block";
- 
-  //document.querySelector('.layer2').style.display = "none";
-  
     
 
 }
@@ -136,25 +78,18 @@ window.onclick = function(event) {
 }
 
 
-
-
-
-
-  
-
-
-
-  //document.querySelector('.layer2').style.display = "none";
-
 function toggleCrown() {
   document.getElementById("baseLizard").style.visibility = "visible";
-    if (document.getElementById("baseLizard").src == "https://live.staticflickr.com/2940/14431710441_385928e588_k.jpg"){
-        document.getElementById("baseLizard").src = "https://user-images.githubusercontent.com/58008006/184746854-52163985-60a2-4ecb-a207-e1da9f9162aa.png";
+    if (document.getElementById("baseLizard")
+        .src == "https://live.staticflickr.com/2940/14431710441_385928e588_k.jpg"){
+        document.getElementById("baseLizard")
+        .src = "https://user-images.githubusercontent.com/58008006/184746854-52163985-60a2-4ecb-a207-e1da9f9162aa.png";
     } else {
-        document.getElementById("baseLizard").src = "https://live.staticflickr.com/2940/14431710441_385928e588_k.jpg";
+        document.getElementById("baseLizard")
+        .src = "https://live.staticflickr.com/2940/14431710441_385928e588_k.jpg";
     }
 
-    //document.querySelector('.layer2').style.display = "block";
+    
     modal.style.display = "none"; 
     
   }
